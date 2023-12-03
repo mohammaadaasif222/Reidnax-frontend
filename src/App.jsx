d9f7bd4d08caa46d45e1e6b2003edc41e4775b8d
Mohammad Aasif <kasifsaif784@gmail.com>
2023-12-03 18:54:17 +0530

    add not found

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import React from "react";
 import SignIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
 import Header from "./components/Header"
+import NotFound from "./pages/NotFound";
 export default function App() {
   return (
     <BrowserRouter>
@@ -12,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/dashboard" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
